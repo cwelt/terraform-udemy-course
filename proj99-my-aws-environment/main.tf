d851120613088be3a26e50e769d40360c4e2861a
Chanan Welt <chwelt88@gmail.com>
2026-01-10 14:18:24 +0200

    add project 99

---
+++ proj99-my-aws-environment/main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+/* loads an AMI for the most recent Ubuntu Noble Numbat release 
+   in the region configured provider region */
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical Account ID for ensuring official Ubuntu AMIs
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "learn-terraform"
+  }
+}
