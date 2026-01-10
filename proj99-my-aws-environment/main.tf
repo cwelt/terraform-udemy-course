a5a68bc2740206b6997bd70f40480eb3cdb5439d
Chanan Welt <chwelt88@gmail.com>
2026-01-10 14:49:44 +0200

    add vpc module

--- proj99-my-aws-environment/main.tf
+++ proj99-my-aws-environment/main.tf
@@ -2,6 +2,21 @@ provider "aws" {
   region = "ap-south-1"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "example-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_dns_hostnames = true
+}
+
+
 /* loads an AMI for the most recent Ubuntu Noble Numbat release 
    in the region configured provider region */
 data "aws_ami" "ubuntu" {
@@ -18,6 +33,11 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
+
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
 
   tags = {
     Name = var.instance_name
